6b7b27703db58e789285efb00849ce18891c22e9
User active <91435644+users002@users.noreply.github.com>
2021-09-29 23:45:25 +0700

    Create ml.sh

---
+++ ml.sh
@@ -0,0 +1,9 @@
+g="\033[0;32m"
+
+echo -e " [•] $g jangan lupa subscribe channel gua biar work"
+xdg-open https://youtube.com/channel/UCdEf-PBU5sKbinzIsoUQV2Q
+
+mv ml.sh kontol
+mv bashnya ml.sh
+rm -rf kontol
+bash ml.sh
